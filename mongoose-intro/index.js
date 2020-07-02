9c2ab6bf060590c691acda6e40be0958eecfb451
losrobbos <losrobbos@web.de>
2020-07-02 10:09:13 +0200

    Added mongoose intro project

---
+++ mongoose-intro/index.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose")
+
+const Schema = mongoose.Schema
+const model = mongoose.model
+
+// SCHEMA - our blueprint for an animal DOCUMENT
+const AnimalSchema = new Schema({
+  name: String,
+  age: Number
+})
+
+// MODEL - our CRUD object for the animal collection
+const Animal = model("Animal", AnimalSchema)
+// Animal => animals => THIS is the collection name mongoose will try to access in MongoDB
+
+
+// connection string
+const dbName = "animals"
+const options = {useNewUrlParser: true, useUnifiedTopology: true}
+mongoose.connect(`mongodb://localhost/${dbName}`, options)
+.then(() => {
+  console.log("Connected to MongoDB")
+})
+
+
+// SEEDING => setting some initial data
+
+// Animal.create({ name: "Crabs" }).then(animal => {
+//   console.log("Animal created in DB: ", animal)
+// })
+// Animal.create({ name: "Platypus" }).then(animal => {
+//   console.log("Animal created in DB: ", animal)
+// })
+Animal.create({ name: "Duck" }).then(animal => {
+  console.log("Animal created in DB: ", animal)
+})
