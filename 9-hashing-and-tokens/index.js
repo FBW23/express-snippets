78888f6bd5c5c80f772cd5412af23036d207b67e
losrobbos <losrobbos@web.de>
2020-07-15 10:10:41 +0200

    Added bcrypt sample

---
+++ 9-hashing-and-tokens/index.js
@@ -0,0 +1,44 @@
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+let holySecret = "hackMeIfYouCan"
+
+// potential new user in our system
+let user = { _id: "123", email: "rob@dci.com", pwPlain: "hello123" }
+
+// hashing a plain text password
+// specify rounds of salting => the higher the longer it takes to create the hash
+let pwHashed = bcrypt.hashSync(user.pwPlain, 10)
+user.pwHashed = pwHashed
+
+console.log("User pws: ", user.pwPlain, pwHashed)
+
+
+/**
+ * Login a user and create a token (=visitor card)
+ */
+const login = (email, pwPlain) => {  
+  if(user.email != email) {
+    console.log("Email does not exist");
+    return;
+  }
+
+  let match = bcrypt.compareSync(pwPlain, user.pwHashed)
+  console.log(match ? "Passwords match" : "Passwords do not match")
+
+  if(!match) {
+    return
+  }
+
+  const token = jwt.sign({ _id: user._id }, holySecret)
+  console.log("Token: ", token)
+  return token
+}
+
+// login("rob@dci.com", "hello333")
+const token = login("rob@dci.com", "hello123")
+
+// /secretRoute => cookie: authToken => eyfjrjajaueuelor$
+
+let decoded = jwt.verify(token, holySecret)
+console.log("Verified token, data received: ", decoded)
