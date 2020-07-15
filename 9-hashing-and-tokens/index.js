9816ca709d7c814026edff71680c81ed64511feb
losrobbos <losrobbos@web.de>
2020-07-15 10:15:07 +0200

    Updated comments

--- 9-hashing-and-tokens/index.js
+++ 9-hashing-and-tokens/index.js
@@ -1,13 +1,15 @@
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
+// secret used for JWT signing & verifying
 let holySecret = "hackMeIfYouCan"
 
 // potential new user in our system
 let user = { _id: "123", email: "rob@dci.com", pwPlain: "hello123" }
 
 // hashing a plain text password
-// specify rounds of salting => the higher the longer it takes to create the hash
+// specify rounds of salting => the higher this number the longer it takes to create the hash
+// making it harder for attackers to calculate all possible combinations
 let pwHashed = bcrypt.hashSync(user.pwPlain, 10)
 user.pwHashed = pwHashed
 
@@ -23,6 +25,7 @@ const login = (email, pwPlain) => {
     return;
   }
 
+  // check if the password given matches with the stored hashed one
   let match = bcrypt.compareSync(pwPlain, user.pwHashed)
   console.log(match ? "Passwords match" : "Passwords do not match")
 
@@ -30,6 +33,7 @@ const login = (email, pwPlain) => {
     return
   }
 
+  // user authenticated => create a token (=visitor card)
   const token = jwt.sign({ _id: user._id }, holySecret)
   console.log("Token: ", token)
   return token
@@ -38,8 +42,7 @@ const login = (email, pwPlain) => {
 // login("rob@dci.com", "hello333")
 const token = login("rob@dci.com", "hello123")
 
-// /secretRoute => cookie: authToken => eyfjrjajaueuelor$
-
+// read the information from the token
 let decoded = jwt.verify(token, holySecret)
 console.log("Verified token, data received: ", decoded)
 
